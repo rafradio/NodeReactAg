ad2e72e61ad830e5a77cbb96362f534b237411e6
Rafail Abdyushev <rafaelab@mail.ru>
2023-06-11 08:49:47 +0300

    ag grid

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, createContext, useContext } from 'react';
+import React, { useState, useEffect, createContext, useContext, useRef, useCallback, useMemo } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import './stylesAg.css';
+import { CellClickedEvent } from "ag-grid-community";
 import { Button } from '@mui/material';
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -16,36 +17,46 @@ import AssideMain from "./Aside.js"
 
 const AppAg = (props) => {
   const dataTable = useContext(props.context);
-  // const [rowData, setData] = useState([]);
+  const gridRef = useRef();
+
   var rowData = dataTable;
 
   let columsName = props.columsName;
-  // const [columnDefs, setColumns] = useState([
-  //     { field: 'id' },
-  //     { field: 'firstName' },
-  //     { field: 'lastName' }
-  // ]);
-
-  // const [columnDefs, setColumns] = useState([]);
 
   let myData = props.data;
 
+  const defaultColDef = useMemo(() => {
+    return {
+      flex: 1,
+      editable: true,
+      sortable: true,
+      filter: true,
+    };
+  }, []);
+
+  const onCellClicked = (CellClickedEvent) => console.log('Cell was clicked');
+  const onGridReady = useCallback(() => {
+    gridRef.current.api.showLoadingOverlay();
+  }, []);
   // useEffect(() => {
   //   setData(dataTable);
   // }, []);
 
   return (
       <div className='container_grid'>
-        <Button variant="contained">Contained</Button>
         <div className="ag-theme-alpine grid_style" style={{height: 400, width: 600}}>
             <AgGridReact
+                onCellClicked={onCellClicked}
+                onGridReady={onGridReady}
+                className="ag-theme-alpine"
+                ref={gridRef}
+                animateRows={true}
                 rowData={myData}
                 columnDefs={columsName}>
             </AgGridReact>
         </div>
-        <Button variant="contained">Contained</Button>
       </div>
-);
+  );
 };
 
 export default AppAg;
--- front/src/Aside.js
+++ front/src/Aside.js
@@ -14,20 +14,21 @@ const UserContext = createContext();
 
 const AssideMain = (props) => {
     let columnsName = [
-        { field: 'id' },
+        { field: 'id', maxWidth: 80 },
         { field: 'bookName' },
         { field: 'writer' }
     ];
     
-    let dataTable = [
-        {id: "VW", firstName: "Celica", lastName: 35000},
-        {id: "Ford", firstName: "Mondeo", lastName: 32000},
-        {id: "Porsche", firstName: "Boxster", lastName: 72000}
-    ];
+    let dataTable = [];
+    // let dataTable = [
+    //     {id: "VW", firstName: "Celica", lastName: 35000},
+    //     {id: "Ford", firstName: "Mondeo", lastName: 32000},
+    //     {id: "Porsche", firstName: "Boxster", lastName: 72000}
+    // ];
 
     const [dataT, setData] = useState(dataTable);
     const [columN, setColumn] = useState(columnsName);
-    // const [request, setRequest] = useState('join')
+    
 
     useEffect(() => {
         takeData('join').then(myData => {
@@ -43,18 +44,6 @@ const AssideMain = (props) => {
         });
 
     }, []);
-
-    // takeData(request).then(myData => {
-    //     let dataTableNew1 = [];
-    //     myData.forEach(element => {
-    //         dataTableNew1.push(
-    //             {id: element.id, bookName: element.bookname, writer: element.writer}
-    //         )
-    //     });
-    //     console.log("main module", dataTableNew1);
-    //     // setColumn(columnNew);
-    //     setData(dataTableNew1);
-    // });
     
 
     const makeAction = (tableName) => {
@@ -92,11 +81,9 @@ const AssideMain = (props) => {
             
         });
 
-        // makeAction('join');
         
     }
 
-    // makeAction('join');
 
     return (
         <UserContext.Provider value={dataT}>
@@ -115,7 +102,7 @@ const AssideMain = (props) => {
 }
 
 const takeData = async (tableName) => {
-    // const textData = '';
+  
     try {
         let url = window.location.href + "data/" + tableName;
         console.log("React ", url);
